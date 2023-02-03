ec5835627ec8b65e7a6b0eb678b5eeede43908d2
Jack Finnis <jack.finnis@icloud.com>
2023-02-03 13:32:12 +0000

    Initial Commit

---
+++ Games/GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesApp.swift
+//  Games
+//
+//  Created by Jack Finnis on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
